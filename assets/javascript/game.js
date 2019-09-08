8b38373ba012804d002704c92cf28ba3ea4b0e83
Fauz Ahmed <fauz.ahmed369@gmail.com>
2019-09-08 17:16:03 -0500

    Decided not to use bootstrap to style my webpage.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,37 @@
-var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-    
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
 var wins = 0;
 var losses = 0;
 var guessesLeft = 10;
+var letterUser = [];
+var eachofLetters = null;
 
-
-
-var computerGuesses = computerChoices[Math.floor(math.random()*computerChoices.length)];
-
-    
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+function countGuessesLeft() {
+    document.querySelector("#left").innerHTML = "Guesses Left: " + guessesLeft;
+}
+function farUserGuesses() {
+    document.querySelector("#guesses").innerHTML = "Your Guesses so far: " + letterUser.join(" ");
+}
+countGuessesLeft();
+var restart = function () {
+    guessesLeft = 10;
+    letterUser = [];
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+}
+document.onkeyup = function (event) {
+    guessesLeft--;
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+    letterUser.push(userGuess);
+    countGuessesLeft();
+    farUserGuesses();
+    if (userGuess === computerGuess) {
+        wins++;
+        document.querySelector("#win").innerHTML = "Wins: " + wins;
+        restart();
+    } else if (guessesLeft === 0) {
+        losses++;
+        document.querySelector("#lose").innerHTML = "Loses: " + losses;
+        restart();
+    }
+};
