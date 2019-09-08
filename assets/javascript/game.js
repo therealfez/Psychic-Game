61551468fb5ad47bdff6b11e211f77c41f07a444
Fauz Ahmed <fauz.ahmed369@gmail.com>
2019-09-08 15:48:54 -0500

    added variables in for wins, losses, guesses left, and computer guesses.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,11 @@
 var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
     
-    
-    
+var wins = 0;
+var losses = 0;
+var guessesLeft = 10;
+
+
+
+var computerGuesses = computerChoices[Math.floor(math.random()*computerChoices.length)];
+
     
